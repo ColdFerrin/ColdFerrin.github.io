6ac0a41dffd3b211f9ad2f4e5cc2dcb3d244e785
ColdFerrin <ferrinkatz@ferrinkatz.com>
2020-04-30 23:09:16 +0000

    Update master to output generated at c5e1d57

--- precache-manifest.adfbab491daca559b43d712fd76ee6eb.js
+++ precache-manifest.adfbab491daca559b43d712fd76ee6eb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "656e1e375a703491af6dec2ed7be6a09",
+    "revision": "a84eb36ab2af74f79172ae12c7122ed3",
     "url": "/index.html"
   },
   {
-    "revision": "261648c4e1e02318af6d",
-    "url": "/static/css/main.16f05fe9.chunk.css"
+    "revision": "5e574dccf549ae09b636",
+    "url": "/static/css/main.96c3e0a2.chunk.css"
   },
   {
-    "revision": "a9561653727e82937c0e",
-    "url": "/static/js/2.d3b2cb59.chunk.js"
+    "revision": "1ed322cc2ee7c538ba95",
+    "url": "/static/js/2.a6bd2d19.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.d3b2cb59.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a6bd2d19.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "261648c4e1e02318af6d",
-    "url": "/static/js/main.0eb4d2ec.chunk.js"
+    "revision": "5e574dccf549ae09b636",
+    "url": "/static/js/main.030698a4.chunk.js"
   },
   {
     "revision": "93cba054f0f94a4ba97d",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "64faaf6b581e293beb1a5197ae604420",
     "url": "/static/media/Ferrin.64faaf6b.png"
+  },
+  {
+    "revision": "659fe4649cd688f90346d9018824597e",
+    "url": "/static/media/Sketch003.659fe464.png"
   },
   {
     "revision": "d127c556403bfdf0f806ebb7964c6e81",
